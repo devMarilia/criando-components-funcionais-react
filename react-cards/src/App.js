dff60c0ce9950544200247963093e68b651c1340
devMarilia <devMarilia.frontend@gmail.com>
2021-06-04 21:47:38 -0300

    :checkered_flag: release

--- react-cards/src/App.js
+++ react-cards/src/App.js
@@ -1,10 +1,30 @@
-import './App.css';
+import '../src/styles/global.css'
+import { Container } from '../src/components/ElementsCard';
+import Card from '../src/components/Card'
 
 function App() {
   return (
-    <div className="App">
-        <h1>Card</h1>
-    </div>
+    <>
+    <head>
+ 
+    <link rel="preconnect" href="https://fonts.gstatic.com"/>
+    <link href="https://fonts.googleapis.com/css2?family=Oxygen:wght@300;400;700&display=swap" rel="stylesheet"/>
+  
+  </head>
+    <Container>
+      <Card img="https://images.unsplash.com/photo-1536304929831-ee1ca9d44906?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+        title="The Everyday Salad"
+        description="Take your boring salads up a knotch. This recipe is perfect for lunch
+          and only contains 5 ingredients!"/>
+      <Card img="https://images.unsplash.com/photo-1476124369491-e7addf5db371?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+        title="Simple Risotto"
+        description="Fear Risotto no more! This simple recipe is perfect for family dinners."/>
+      <Card img="https://images.unsplash.com/photo-1529928520614-7c76e2d99740?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+        title="Baked Cod with Vegetables"
+        description="Baked Cod with Vegetables. 30 minute meal!"/>
+      
+    </Container>
+    </>
   );
 }
 
